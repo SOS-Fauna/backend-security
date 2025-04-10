6d1b64078c0b16f27e4ad5a83b3ba2e18f958ec5
helelys <tallyslabanca20@gmail.com>
2025-04-10 00:07:57 -0300

    proibindo que o usuário digite algum email inválido no back

--- src/main/java/com/security/demo/controller/AuthController.java
+++ src/main/java/com/security/demo/controller/AuthController.java
@@ -4,7 +4,9 @@ import com.security.demo.model.dto.*;
 import com.security.demo.service.AuthService;
 import com.security.demo.service.RegisterService;
 import lombok.AllArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -12,19 +14,38 @@ import org.springframework.web.bind.annotation.*;
 @AllArgsConstructor
 public class AuthController {
 
+    private static final Logger log = LoggerFactory.getLogger(AuthController.class);
     private AuthService authService;
     private RegisterService registerService;
 
     @PostMapping("/register/user")
-    public String registerUser(@RequestBody RegistrarUsuarioDTO body) {
-        registerService.registerUser(body.getEmail(), body.getSenha());
-        return "User registered successfully";
+    public ResponseEntity<String> registerUser(@RequestBody RegistrarUsuarioDTO body) {
+        try {
+            if (!body.getEmail().matches("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$")) {
+                throw new IllegalArgumentException("Email de usuário inválido");
+            }
+            registerService.registerUser(body.getEmail(), body.getSenha());
+            return ResponseEntity.ok("Usuário registrado com sucesso!");
+        }
+        catch (IllegalArgumentException e) {
+            log.warn("Erro ao registrar email de usuário", e.getMessage());
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @PostMapping("/register/ong")
-    public String registerOng(@RequestBody RegistrarOrgaoDTO body) {
-        registerService.registerOng(body.getEmail(), body.getSenha());
-        return "ONG registered successfully";
+    public ResponseEntity<String> registerOng(@RequestBody RegistrarOrgaoDTO body) {
+        try {
+            if (!body.getEmail().matches("^[^\\s@]+@[^\\s@]+\\.[^\\s@]+$")) {
+                throw new IllegalArgumentException("Email de ONG inválido");
+            }
+            registerService.registerOng(body.getEmail(), body.getSenha());
+            return ResponseEntity.ok("ONG registrada com sucesso!");
+        }
+        catch (IllegalArgumentException e) {
+            log.warn("Erro ao registrar email de ONG", e.getMessage());
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
     }
 
     @PostMapping("/login/user")
